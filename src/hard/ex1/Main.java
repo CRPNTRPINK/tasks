2ec9d482655d73e2af5b6c11c04b7dad0d3a93f6
CRPNTRPINK <ultrololl@gmail.com>
2021-06-03 06:44:39 +0300

    update

--- src/hard/ex1/Main.java
+++ src/hard/ex1/Main.java
@@ -33,14 +33,13 @@ public class Main {
         user1.addRole(role2);
         user3.addRole(role3);
 
-
         System.out.println("\n\nНеобходимо разработать метод, который получал на вход Код пользователя и Код Полномочия и возвращал бы true/false, в зависимости, есть ли такое полномочие у пользователя или нет\n");
         System.out.println(Methods.findUserAndCredential(1, 10)); // полномочия с кодом 10 не существует
         System.out.println(Methods.findUserAndCredential(1, 2)); // у пользователя с id 1 есть полномочие с id 2
         System.out.println(Methods.findUserAndCredential(3, 1)); // у пользователя с id 3 нет права создавать записи
 
         System.out.println("\n\nНеобходимо разработать метод, который возвращает список всех доступных пользователю полномочий\n");
-        System.out.println(Methods.findAllUserCredentials(1)); // передаем id пользователя
+        System.out.println(Methods.findAllUserCredentials(2)); // передаем id пользователя
 
 
     }
--- src/hard/ex1/Methods.java
+++ src/hard/ex1/Methods.java
@@ -7,19 +7,19 @@ public class Methods {
     private static List<User> users = new ArrayList<>();
 
     public static void setUsers(User user) {
-       users.add(user);
+        users.add(user);
     }
 
     public static List<User> getUsers() {
         return users;
     }
 
-    public static boolean findUserAndCredential(int userId, int credentialId){
-        for (User user: users){
+    public static boolean findUserAndCredential(int userId, int credentialId) {
+        for (User user : users) {
             if (user.getId() == userId) {
                 for (Role role : user.getRoles()) {
                     for (Credentials credential : role.getCredentials()) {
-                        if (credential.getId() == credentialId){
+                        if (credential.getId() == credentialId) {
                             return true;
                         }
                     }
@@ -29,21 +29,25 @@ public class Methods {
         return false;
     }
 
-    public static String findAllUserCredentials(int userId){
+    public static String findAllUserCredentials(int userId) {
         String result = new String();
         List<String> credentialNames = new ArrayList<>();
-        for (User user: users){
+        for (User user : users) {
             if (user.getId() == userId) {
                 result += "Полномочия пользователя \"" + user.getName() + "\":\n";
-                for (Role role : user.getRoles()) {
-                    for (Credentials credential : role.getCredentials()) {
-                        if (!credentialNames.contains(credential.getName())){
-                            credentialNames.add(credential.getName());
-                        }
+                if (user.getRoles() != null) {
+                    for (Role role : user.getRoles()) {
+                        for (Credentials credential : role.getCredentials()) {
+                            if (!credentialNames.contains(credential.getName())) {
+                                credentialNames.add(credential.getName());
+                            }
                         }
                     }
+                }else {
+                    result += "нет полномочий";
                 }
             }
+        }
         result += String.join("\t|\t", credentialNames);
         return result;
     }
